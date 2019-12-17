beaedc2e681d5379c3025da8539b890568b3cd8f
AswinTA <aswinta98@gmail.com>
2019-12-17 11:39:58 +0530

    removed commented animation and ajax fetch from deaf.js

--- dashboard/src/js/c3js/deaf.js
+++ dashboard/src/js/c3js/deaf.js
@@ -1,75 +1,22 @@
 
 
-function setDeafDonut(total,yes){
+function setDeafDonut(total, yes) {
   let no = total - yes;
   var chart = c3.generate({
     bindto: "#deaf-chart",
     data: {
-        columns: [
-            ['Yes',yes],
-            ['No',no],
-        ],
-        type : 'donut',
-        onclick: function (d, i) { console.log("onclick", d, i); },
-        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+      columns: [
+        ['Yes', yes],
+        ['No', no],
+      ],
+      type: 'donut',
+      onclick: function (d, i) { console.log("onclick", d, i); },
+      onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+      onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     },
     donut: {
-        title: "Deaf"
+      title: "Deaf"
     }
-});
+  });
 }
-
-
-
-
-
-
-// setTimeout(function () {
-//     chart.load({
-//         columns: [
-//             ["Yes", 0.9, 0.2, 0.9, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.9, 0.2, 0.2, 0.9, 0.9, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
-//             ["No", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
-//         ]
-//     });
-// }, 1500);
-
-// setTimeout(function () {
-//     chart.unload({
-//         ids: 'data1'
-//     });
-//     chart.unload({
-//         ids: 'data2'
-//     });
-// }, 2500);
-document.onload = loadDeafData();
-
-async function loadDeafData(){
-
-    let url ="http://192.168.35.34:7555/";
-    let jsonData = new Object();
-    jsonData.affectedUsers = "Deaf";
-    let response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jsonData)
-      });
-    if (response.ok) { // if HTTP-status is 200-299
-      // get the response body (the method explained below)
-      let json = await response.json();
-      console.log(json)
-      let total = json.Total;
-      let yes = json.Yes;
-      setDeafDonut(total, yes);
-
-    } else {
-      alert("HTTP-Error: " + response.status);
-      // setDeafDonut(total, yes);
-    }
-}
-
-
 export default setDeafDonut;
