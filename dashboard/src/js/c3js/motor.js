95f5f99d8b0e342bcceea2b8ad8c636d893a6dcc
AswinTA <aswinta98@gmail.com>
2019-12-17 12:16:07 +0530

    removed commented code for animation. wrapped the chart initialization inside a function and export changed the data variable

--- dashboard/src/js/c3js/motor.js
+++ dashboard/src/js/c3js/motor.js
@@ -1,35 +1,24 @@
-var chart = c3.generate({
-    bindto: "#motor-chart",
-    data: {
-        columns: [
-            ['Yes',40],
-            ['No', 120],
-        ],
-        type : 'donut',
-        onclick: function (d, i) { console.log("onclick", d, i); },
-        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
-    },
-    donut: {
-        title: "Motor"
-    }
-});
 
-// setTimeout(function () {
-//     chart.load({
-//         columns: [
-//             ["data1", 0.9, 0.2, 0.9, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.9, 0.2, 0.2, 0.9, 0.9, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
-//             ["data2", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
-//         ]
-//     });
-// }, 1500);
 
-// setTimeout(function () {
-//     chart.unload({
-//         ids: 'data1'
-//     });
-//     chart.unload({
-//         ids: 'data2'
-//     });
-// }, 2500);
+function setMotorDonut(total,yes){
+    let no = total - yes;
+    var chart = c3.generate({
+        bindto: "#motor-chart",
+        data: {
+            columns: [
+                ['Yes',yes],
+                ['No', no],
+            ],
+            type : 'donut',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+        },
+        donut: {
+            title: "Motor"
+        }
+    });
+}
 
+
+export default setMotorDonut;
