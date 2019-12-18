66f343c3e27e1c144c880afe8dcd13323ac8ccbc
AswinTA <aswinta98@gmail.com>
2019-12-18 13:56:59 +0530

    chart all updated with all feature

--- dashboard/src/js/c3js/allCharts.js
+++ dashboard/src/js/c3js/allCharts.js
@@ -1,27 +1,50 @@
-function setAllDonutChart(total, yes){
+function setAllDonutChart(total, yes) {
     let no = total - yes;
     const allChart = c3.generate({
         bindto: "#all-chart",
-        data: {
-            columns: [
-                ['Yes',30],
-                ['No', 60],
-            ],
-            type : 'donut',
-            onclick: function (d, i) { console.log("onclick", d, i); },
-            onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-            onmouseout: function (d, i) { console.log("onmouseout", d, i); },
-
-            colors: {
-                Yes:'#000000',
-                No: '#99965c'
-              },
-              labels: false
+        // all features
+        size: {
+            width: 400,
+            height: 400
         },
+        padding: {
+            top: 50,
+            left: 50,
+            right: 50,
+            bottom: 50
+        },
+        // color: {
+        //     pattern: ['#1f77b4', '#aec7e8']
+        //   },
+        transition: {
+            duration: 500
+        },
+        oninit: function () { console.log("Chart initialized") },
+        onrendered: function () {console.log("Chart is rendered") },
+        onresize: function () { console.log("Screen is resized")},
+
+
+        // all features
+        data: {
+        columns: [
+            ['Yes', 30],
+            ['No', 60],
+        ],
+        type: 'area',
+        onclick: function (d, i) { /* console.log("onclick", d, i); */ },
+        onmouseover: function (d, i) {/*  console.log("onmouseover", d, i); */ },
+        onmouseout: function (d, i) { /* console.log("onmouseout", d, i); */ },
+
+        // colors: {
+        //     Yes: '#000000',
+        //     No: '#99965c'
+        // },
+        labels: false
+    },
         donut: {
-            title: "All",
-            padAngle : .03
-        }
+        title: "All",
+        padAngle: .04
+    }
         // size:{
         //     width:200,
         //     height:200
